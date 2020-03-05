b73b7a05d1dad49cd6386717bc0e6c8b696c3357
avanwyk <abvanwyk@gmail.com>
2020-03-05 10:43:30 +0200

    Add language support for hello
    Refactor hello and tests.

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,35 @@ package main
 
 import "fmt"
 
-func Hello(entity string) string {
-	return fmt.Sprintf("Hello, %s", entity)
+const es = "Spanish"
+const fr = "French"
+const af = "Afrikaans"
+const enGreetingPrefix = "Hello, "
+const esGreetingPrefix = "Hola, "
+const frGreetingPrefix = "Bonjour, "
+const afGreetingPrefix = "Goeiedag, "
+
+func Hello(entity string, language string) string {
+	if entity == "" {
+		entity = "World"
+	}
+	return greetingPrefix(language) + entity
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case es:
+		prefix = esGreetingPrefix
+	case fr:
+		prefix = frGreetingPrefix
+	case af:
+		prefix = afGreetingPrefix
+	default:
+		prefix = enGreetingPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,16 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestHello(t *testing.T) {
-	name := "Chris"
-	got := Hello(name)
-	want := fmt.Sprintf("Hello, %s", name)
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, expected string, actual string) {
+		t.Helper()
+		if actual != expected {
+			t.Errorf("Expected message %q, actual message %q", expected, actual)
+		}
 	}
+
+	t.Run("should greet people", func(t *testing.T) {
+		name := "Chris"
+		actual := Hello(name, "")
+		expected := "Hello, " + name
+		assertCorrectMessage(t, expected, actual)
+	})
+
+	t.Run("should greet world if no name is given", func(t *testing.T) {
+		actual := Hello("", "")
+		expected := "Hello, World"
+		assertCorrectMessage(t, expected, actual)
+	})
+
+	t.Run("should greet in Spanish", func(t *testing.T) {
+		actual := Hello("Julia", "Spanish")
+		expected := "Hola, Julia"
+		assertCorrectMessage(t, expected, actual)
+	})
+
+	t.Run("should greet in French", func(t *testing.T) {
+		actual := Hello("Andre", "French")
+		expected := "Bonjour, Andre"
+		assertCorrectMessage(t, expected, actual)
+	})
+
+	t.Run("should greet in Afrikaans", func(t *testing.T) {
+		actual := Hello("Pieter", "Afrikaans")
+		expected := "Goeiedag, Pieter"
+		assertCorrectMessage(t, expected, actual)
+	})
 }
